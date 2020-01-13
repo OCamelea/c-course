52770154c0d20ec17d89ac0c5b0969de9263b7a6
unknown <camelea.ouarkoub.etu@univ-lille.fr>
2020-01-13 15:34:20 +0100

    ok

---
+++ CSharpExercices-master/CSharpExercices-master/src/CS.Impl/01_Basic/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CS.Impl._01_Basic
+{
+    public class Math
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Divide(int a, int b)
+        {
+          return a / b;
+        }
+
+        public int SumTable(IEnumerable<int> integersTable)
+        {
+            int sum = 0;
+            foreach( int element in integersTable)
+            {
+                sum += element;
+            }
+            return sum;
+        }
+    }
+}
+
---
+++ CSharpExercices-master/CSharpExercices-master/src/CS.Impl/01_Basic/Sentence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CS.Impl._01_Basic
+{
+    public class Sentence
+    {
+        public string Reverse(string sentence)
+        {
+            String[] words = sentence.Split(' ');
+            Array.Reverse(words);
+            return String.Join(" ", words);
+
+                }
+    }
+}
